88f92d800660830529dadd651d04299b822c7fd2
IT17101370_NETHMEE <IT17101370@my.sliit.lk>
2020-10-06 02:23:02 +0530

    bayes_model

---
+++ frontend/Frontend/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/patient', title: 'Patients',  icon:'ni-single-02 text-blue', class: '' },
+    { path: '/pregnancy', title: 'Pregnancy Entries',  icon:'ni-badge text-blue', class: '' },
+    { path: '/records', title: 'Records',  icon:'ni-bullet-list-67 text-blue', class: '' },
+    // { path: '/tables', title: 'Analytics',  icon:'ni-sound-wave text-blue', class: '' },
+    // { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
+    // { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  public menuItems: any[];
+  public isCollapsed = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+   });
+  }
+}
